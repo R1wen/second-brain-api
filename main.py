ca24158e847399155930624d4792123ac4a317d0
Erwin K. OKE <okekomerwin@gmail.com>
2026-03-02 23:04:13 +0000

    feat: added text extraction from PDF feature

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Ion know0")
+import os
+
+import pymupdf
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def extract_text_from_pdf(file_path: str) -> str:
+    text = ""
+    doc = pymupdf.open(file_path)
+    for page in doc:
+        text += page.get_text()
+    return text
+
+
+if __name__ == "__main__":
+    path = "./test.pdf"
+    result = extract_text_from_pdf(path)
+    print(result)
