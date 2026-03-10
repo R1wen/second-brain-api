8b3c1f0abede8cf8bc722c5cb8dac638f0c4f6ea
Erwin K. OKE <okekomerwin@gmail.com>
2026-03-10 20:04:20 +0000

    feat: added chunking logic with overlap

--- main.py
+++ main.py
@@ -14,7 +14,30 @@ def extract_text_from_pdf(file_path: str) -> str:
     return text
 
 
+def chunk_text(text: str, chunk_size: int = 1000, overlap: int = 200) -> list[str]:
+    chunks = []
+    start = 0
+    text_length = len(text)
+
+    while start < text_length:
+        end = start + chunk_size
+        chunk = text[start:end]
+        chunks.append(chunk)
+        start = end - overlap
+    return chunks
+
+
 if __name__ == "__main__":
     path = "./test.pdf"
+    print("extraction...")
     result = extract_text_from_pdf(path)
-    print(result)
+    print(f"total characters length: {len(result)}")
+
+    print("chunking...")
+    chunks = chunk_text(result)
+    print(f"number of chunks : {len(chunks)}")
+
+    if len(chunks) >= 2:
+        print(chunks[0][-200:])
+        print("----2eme-----\n")
+        print(chunks[1][:200])
